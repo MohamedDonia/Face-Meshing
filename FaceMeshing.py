774a203276e220848f0e50f7c527e6b9e27fcb22
Mohamed Mahmoud Donia <81274360+MohamedDonia@users.noreply.github.com>
2021-06-23 19:18:52 +0200

    Add files via upload

--- FaceMeshing.py
+++ FaceMeshing.py
@@ -5,13 +5,20 @@ from mediapipe.python.solutions.drawing_utils import draw_landmarks, DrawingSpec
 
 
 cap = cv2.VideoCapture('videos/7.mp4')
+scale = 1
+frame_width = int(cap.get(3) * scale)
+frame_height = int(cap.get(4) * scale)
+# is stored in 'filename.avi' file.
+out = cv2.VideoWriter('result.avi',
+                      cv2.VideoWriter_fourcc(*'MJPG'),
+                      20, (frame_width, frame_height))
 mymesh = FaceMesh(static_image_mode=False,
                   max_num_faces=1,
                   min_detection_confidence=0.5,
                   min_tracking_confidence=0.5)
 mydrawingspecs = DrawingSpec(thickness=2, circle_radius=1)
 pTime = time.time()
-scale = 0.8
+
 while True:
     success, img = cap.read()
     if not success:
@@ -33,5 +40,10 @@ while True:
     cv2.putText(img, str(int(fps)),
                 (20, 50), cv2.FONT_HERSHEY_PLAIN,
                 3, (0, 255, 0), 3)
+    out.write(img)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
